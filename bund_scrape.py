09cc18fb77b033d72e9e87abae06e787acb828b5
patrickvossler18 <pvossler@usc.edu>
2015-08-15 00:58:49 -0700

    Add way to differentiate standings from different weeks

--- bund_scrape.py
+++ bund_scrape.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import time 
+from datetime import *
+
 
 '''
 German Results
@@ -32,13 +35,50 @@ data = r.text
 soup = BeautifulSoup(data, "html.parser")
 
 table = soup.find('table')
+week = 1
+today = date.today()
+timechange = timedelta(days=7)
+print today
+week2 = datetime.strptime('08/17/15', "%m/%d/%y")
+week3 = week2 + timechange
+week4 = week3 + timedelta(days=7)
+week5 = week4 + timedelta(days=7)
+week6 = week5 + timedelta(days=7)
+week7 = week6 + timedelta(days=7)
+week8 = week7 + timedelta(days=7)
+week9 = week8 + timedelta(days=7)
+week10 = week9 + timedelta(days=7)
+week11 = week10 + timedelta(days=7)
+week12 = week11 + timedelta(days=7)
+week13 = week12 + timedelta(days=7)
+week14 = week13 + timedelta(days=7)
+week15 = week14 + timedelta(days=7)
+week16 = week15 + timedelta(days=7)
+week17 = week16 + timedelta(days=7)
+week18 = week17 + timedelta(days=7)
+week19 = week18 + timedelta(days=7)
+week20 = week19 + timedelta(days=7)
+week21 = week20 + timedelta(days=7)
+week22 = week21 + timedelta(days=7)
+
+week_list = [week2, week3,week4,week5,week6,week7,week8 ,week9,   week10,  week11,  week12,  week13,  week14,  week15,  week16,  week17,  week18,  week19,  week20, week21, week22] 
+for x in week_list:
+    print x 
+    if today == week2.date():
+        week += 1
+    print week
+
+
+
+
+filename = 'Bundesliga_Standings_Week_' + str(week) + '.csv'
 
 #table_headers = []
 def get_scores():
     for tx in table.findAll('tr', {'class': "columns"}):
         table_headers = [th.text.strip().encode('utf-8') for th in tx.findAll('th') ] 
 
-    with open('Bundesliga_Standings.csv', 'wb+') as csv_file:
+    with open(filename, 'wb+') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames = table_headers, delimiter = ',')
         writer.writeheader()
         writer = csv.writer(csv_file)
